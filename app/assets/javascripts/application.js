ef4ac359198fdc5e2a7e7bc7045f776dba151b97
Frank Soucy <franksoucy9@gmail.com>
2015-08-12 11:47:02 -0400

    Required bootstrap last

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require bootstrap
 //= require turbolinks
 //= require personal_style.js
+//= require bootstrap
 //= require_tree .

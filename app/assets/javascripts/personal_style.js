0b41e23b291aac9dde32971b3d4e13acb0a7701f
Frank Soucy <franksoucy9@gmail.com>
2016-02-04 23:40:37 -0600

    Search suggestions

--- app/assets/javascripts/personal_style.js
+++ app/assets/javascripts/personal_style.js
@@ -1,4 +1,58 @@
 $(document).ready(function() {
+    //
+    $('.search_string').focusin(function() {
+
+	$('.suggestions').css('display', 'block');
+    });
+
+    $('.search_string').focusout(function() {
+
+	$('.suggestions').css('display', 'none');
+    });
+
+    
+    $('.search_string').keydown(function(event) {
+	
+	var key = event.which;
+	if (key == 38 || key == 40)
+	{
+	    event.preventDefault();
+	}
+	if (key == 40)
+	{
+	    var done = false;
+	    if ($('#sug_list > .active').is('#sug_list > :last-child'))
+	    {
+		$('#sug_list > :last-child').removeClass('active');
+		$('#sug_list > :first-child').addClass('active');
+		done = true;
+	    }
+	    if (!done)
+	    {
+		$('#sug_list > .active').next().addClass('next');
+		$('#sug_list > .active').removeClass('active');
+		$('#sug_list > .next').addClass('active').removeClass('next');  
+	    }
+	}
+	if (key == 38)
+	{
+	    var done = false;
+	    if ($('#sug_list > .active').is('#sug_list > :first-child'))
+	    {
+		$('#sug_list > :first-child').removeClass('active');
+		$('#sug_list > :last-child').addClass('active');
+		done = true;
+	    }
+	    if (!done)
+	    {
+		$('#sug_list > .active').prev().addClass('next');
+		$('#sug_list > .active').removeClass('active');
+		$('#sug_list > .next').addClass('active').removeClass('next');
+	    }
+	}
+	
+    });
+
     //home header
     $('.dropdown_header').mouseenter(function() {
 	$('.dropdown_menu_header').css('display', 'block');
@@ -13,7 +67,7 @@ $(document).ready(function() {
     $('.quick_view').hide();
 
     $('.quick_view').click(function() {
-	var $toPut = "<div class='modal'><p>What the fuk</p></div>";
+	var $toPut = "<div class='modal'><p>What the</p></div>";
 	$('body').append($toPut);
 	
 	//$('body').append(toPut);

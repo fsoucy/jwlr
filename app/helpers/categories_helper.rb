4f49465e3455340b9d563dcd843a8802dfd1a290
James Lovejoy <jameslovejoy1@gmail.com>
2016-02-11 01:21:17 -0600

    Used nested list for category editting.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,14 +1,14 @@
 module CategoriesHelper
 
-  def nested_dropdown(items)
+  def nested_dropdown(items, disabled)
     result = []
     items.each do |item|
-      if item.children.any?
+      if item.children.any? && disabled
         result <<  [('- ' * item.depth) + item.name, item.id, disabled: true]
       else
         result <<  [('- ' * item.depth) + item.name, item.id]
       end
-      result += nested_dropdown(item.children) unless !item.children.any?
+      result += nested_dropdown(item.children, disabled) unless !item.children.any?
     end
     result
   end

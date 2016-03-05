feaa51d1773d5003d1eb8cbc4f408e161d97a624
James Lovejoy <jameslovejoy1@gmail.com>
2016-03-05 00:53:15 -0600

    Start paypal IPN basis.

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,39 @@
+class PaymentsController < ApplicationController
+  protect_from_forgery :except => [:update] #Otherwise the request from PayPal wouldn't make it to the controller
+  
+  def update
+    response = validate_IPN_notification(request.raw_post)
+    case response
+    when "VERIFIED"
+      # check that paymentStatus=Completed
+      # check that txnId has not been previously processed
+      # check that receiverEmail is your Primary PayPal email
+      # check that paymentAmount/paymentCurrency are correct
+      # process payment
+    when "INVALID"
+      # log for investigation
+    else
+      # error
+    end
+    render :nothing => true
+  end 
+ 
+  protected 
+    def validate_IPN_notification(raw)
+      uri = URI.parse('https://www.paypal.com/cgi-bin/webscr?cmd=_notify-validate')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.open_timeout = 60
+      http.read_timeout = 60
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.use_ssl = true
+      response = http.post(uri.request_uri, raw,
+                         'Content-Length' => "#{raw.size}",
+                         'User-Agent' => "My custom user agent"
+                       ).body
+  end
+
+  def new
+
+  end
+
+end

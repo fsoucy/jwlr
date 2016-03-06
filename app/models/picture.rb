1788eee80e01f4d3da15bec1b15b3faca5632e52
James Lovejoy <jameslovejoy1@gmail.com>
2016-03-05 20:19:03 -0600

    Convert all images to png files.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "200x200>" }
+  has_attached_file :photo, :styles => { :medium => ["300x300>", :png], :thumb => ["200x200>", :png] }
   validates_attachment :photo, :presence => true, :content_type => { :content_type => /\Aimage\/.*\Z/ }, :size => { :less_than => 10.megabyte }
 
 

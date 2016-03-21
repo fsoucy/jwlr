39563cb73bfc91cdeb32501401966b71628a6bf8
James Lovejoy <jameslovejoy1@gmail.com>
2016-03-20 22:41:32 -0500

    Minor fixes

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,7 +2,7 @@ class Deal < ActiveRecord::Base
   belongs_to :product
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
-  has_one :selling_method
-  has_one :exchange_method
-  has_one :payment_method
+  belongs_to :selling_method
+  belongs_to :exchange_method
+  belongs_to :payment_method
 end

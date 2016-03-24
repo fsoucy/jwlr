149d8b64aa64bc0f68f4e2205da53356d58bc000
Frank Soucy <franksoucy9@gmail.com>
2016-03-24 17:18:42 -0500

    More messaging

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,12 +1,52 @@
 function refreshMessages()
 {
-    $(".massage_container").load('http://igold.ws:3000/conversations/8' + " .massages");
-    $('.massage_container').scrollTop(10000000);
+    //$(".massage_container").load('http://igold.ws:3000/conversations/8' + " .massages");
+    $.ajax({
+	type: "GET",// GET in place of POST
+	contentType: "application/json; charset=utf-8",
+	url: "http://" + window.location.host + "/conversations/8/pull_messages?page=" + window.page.toString(),
+	data: {},
+	dataType: "json",
+	success: function (result) {
+	    results = result.reverse();
+	    for (res in results)
+	    {
+		thing = results[parseInt(res)];
+		if (window.time != null && window.time < thing[2])
+		{
+		    $('.massages').append('<li>' + '<img src="' + thing[3] + '">' + thing[1] + ': ' + thing[0] + '</li>');
+		    window.time = thing[2]
+		    $('.massage_container').scrollTop(10000000);
+		}
+	    }
+	},
+	error: function (e){
+	}
+    });
 }
 
-$(document).ready(function() {
+$(document).ready(function()
+{
     $('.massage_container').scrollTop(10000000);
-    window.page = 2;
+    window.page = 1;
+    $.ajax({
+	type: "GET",// GET in place of POST
+	contentType: "application/json; charset=utf-8",
+	url: "http://" + window.location.host + "/conversations/8/pull_messages?page=" + window.page.toString(),
+	data: {},
+	dataType: "json",
+	success: function (result) {
+	    results = result.reverse();
+	    for (res in results)
+	    {
+		thing = results[parseInt(res)];
+		$('.massages').prepend('<li>' + '<img src="' + thing[3] + '">' +  thing[1] + ': ' + thing[0] + '</li>');
+		window.time = thing[2];
+	    }
+	},
+	error: function (e){
+	}
+    });
     $('.massage_container').scroll(function(event) {
 	if ($('.massage_container').scrollTop() < 10)
 	{
@@ -22,7 +62,8 @@ $(document).ready(function() {
 		    for (res in results)
 		    {
 			thing = results[parseInt(res)];
-			$('.massages').prepend('<li>' + thing[1] + ': ' + thing[0] + ',' + thing[2] + '</li>');
+			$('.massages').prepend('<li>' + '<img src="' + thing[3] + '">' + thing[1] + ': ' + thing[0] + '</li>');
+			window.time = thing[2];
 		    }
 		},
 		error: function (e){
@@ -38,7 +79,7 @@ $(document).ready(function() {
 	    event.preventDefault();
 	    var result = $.post('/messages', $('#message_form').serialize());
 	    
-	    $(".massage_container").load('http://igold.ws:3000/conversations/8' + " .massages");
+	    //$(".massage_container").load('http://igold.ws:3000/conversations/8' + " .massages");
 	    $('.massage_container').scrollTop(10000000);
 	    $('#message_input').val('');
 	}
@@ -47,7 +88,7 @@ $(document).ready(function() {
     $('#message_submit').click(function(event) {
 	event.preventDefault();
 	$.post('/messages', $('#message_form').serialize());
-	$(".massages").load('http://igold.ws:3000/conversations/8' + " .massages");
+	//$(".massages").load('http://igold.ws:3000/conversations/8' + " .massages");
 	$('.massage_container').scrollTop(10000000);
 	$('#message_input').val('');
     });

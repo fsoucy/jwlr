9a9e2f8bf112afe611f78b26263ad6b31ea5fd3c
James Lovejoy <jameslovejoy1@gmail.com>
2016-03-24 13:57:34 -0500

    Only send relevant data.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -11,8 +11,12 @@ class ConversationsController < ApplicationController
 
   def pull_messages
     conversation = Conversation.find(params[:id])
-    new_messages = conversation.messages.paginate(:page => params[:page], :per_page => 50).order("created_at DESC").reverse
-    render json: new_messages.to_json, status: 200
+    new_messages = conversation.messages.paginate(:page => params[:page], :per_page => 50).order("created_at DESC").pluck(:content, :sender_id).reverse
+    data = Array.new
+    new_messages.each do |message|
+      data.append([message[0], User.find(message[1]).name])
+    end
+    render json: data.to_json, status: 200
   end
 
   private

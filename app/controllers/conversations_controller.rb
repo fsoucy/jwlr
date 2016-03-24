dd5e64e2f2b141c1866288a1b1fd9a8add8c4612
James Lovejoy <jameslovejoy1@gmail.com>
2016-03-24 00:17:12 -0500

    Add some basic validations.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,21 @@
 class ConversationsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+
   def show
     @conversation = Conversation.find(params[:id])
     @messages = @conversation.messages
 
     @msg = @conversation.messages.build
   end
+
+  private
+    
+    def correct_user
+      conversation = Conversation.find(params[:id])
+      if !(current_user.id == conversation.first_user_id || current_user.id == conversation.second_user_id) 
+        redirect_to root_url
+      end
+      return true
+    end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :logged_in_user
+  
   def create
     other_id = params[:message][:other_id]
     user_id = params[:message][:sender_id]
-    conversation = Conversation.where("first_user_id=? or first_user_id=? AND second_user_id=? or second_user_id=?", user_id, other_id, user_id, other_id)
-    if conversation.first.nil?
-      convo = Conversation.new(first_user_id: other_id, second_user_id: user_id)
-      convo.save
+    if current_user.id == user_id
+      conversation = Conversation.where("first_user_id=? or first_user_id=? AND second_user_id=? or second_user_id=?", user_id, other_id, user_id, other_id)
+      if conversation.first.nil?
+        convo = Conversation.new(first_user_id: other_id, second_user_id: user_id)
+        convo.save
+      else
+        convo = conversation.first
+      end
+      @message = convo.messages.build(message_params)
+      @message.save
+      if params[:message][:on_deals]
+        redirect_to Deal.find(params[:message][:deal_id])
+      else
+        redirect_to @message.conversation
+      end
     else
-      convo = conversation.first
-    end
-    @message = convo.messages.build(message_params)
-    @message.save
-    if params[:message][:on_deals]
-      redirect_to Deal.find(params[:message][:deal_id])
-    else
-      redirect_to @message.conversation
+      redirect_to root_url
     end
   end
 

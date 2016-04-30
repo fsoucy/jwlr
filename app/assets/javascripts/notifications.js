3e4ebd6ac49ce549f61804c769530c8f2a9fc7ea
Frank Soucy <franksoucy9@gmail.com>
2016-04-30 02:06:25 -0400

    Notifications AJAX kinda working but really funky things happening

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
     $('.notification').hover(function() {
 	var str = $(this).children('.edit_notification').children('.notification_id').val();
 	console.log(str);
-	var place = '/notifications/' + str;
-	debugger;
+	var place = '/users/2/notifications/' + str;
 	$.post(place, $(this).children('form').serialize());
     });
 });

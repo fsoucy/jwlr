5d502198a61c25b7dd5c9ed10d6439a3287a8cef
Frank Soucy <franksoucy9@gmail.com>
2016-04-30 14:40:25 -0400

    Notifications working

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
     $('.notification').hover(function() {
-	var str = $(this).children('.edit_notification').children('.notification_id').val();
-	console.log(str);
-	var place = '/users/2/notifications/' + str;
-	$.post(place, $(this).children('form').serialize());
+
+	if ($(this).children('.read').length > 0)
+	{
+	    console.log('read');
+	}
+	else
+	{
+	    var str = $(this).children('.edit_notification').children('.n\
+otification_id').val();
+	    var place = '/users/2/notifications/' + str;
+	    $.post(place, $(this).children('form').serialize());
+	    $(this).children('p').removeClass("not_read").addClass("read");
+	    $(this).removeClass('notification_not_read').addClass('notification_read');
+	}
     });
+
+    $('.read').parent('.notification').addClass('notification_read');
+    $('.not_read').parent('.notification').addClass('notification_not_read');
 });

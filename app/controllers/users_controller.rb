da1640a792665fa31a2925a04018a82da23e43a5
Frank Soucy <franksoucy9@gmail.com>
2016-05-05 01:39:32 -0400

    Cropping works now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all.paginate(page: params[:page])
+    @users = User.all.paginate(page: params[:page], per_page: 5)
   end
   
   def show

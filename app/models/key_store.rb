0cece8ec27c089b96b4b7d9220da6f0c5292e0ca
James Lovejoy <jameslovejoy1@gmail.com>
2016-05-07 22:59:37 -0400

    Fix KeyStore bug caused due to constant reloading.

--- app/models/key_store.rb
+++ app/models/key_store.rb
@@ -1,21 +1,21 @@
 class KeyStore < ActiveRecord::Base
 
   def [](key)
-    to_return = KeyStore.where(key: key).first
+    to_return = ::KeyStore.where(key: key).first
     if !to_return.nil?
       to_return.value
     end
   end
 
   def []=(key, value)
-    to_save = KeyStore.where(key: key).first_or_initialize
+    to_save = ::KeyStore.where(key: key).first_or_initialize
     to_save.value = value
     to_save.save
     true
   end
 
   def del(key)
-    to_return = KeyStore.where(key: key).first
+    to_return = ::KeyStore.where(key: key).first
     if !to_return.nil?
       to_return.destroy
       true

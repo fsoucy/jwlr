7c5afe5481746c755d21458df74d406735463343
Frank Soucy <franksoucy9@gmail.com>
2016-05-22 13:45:00 -0400

    Deals javascript

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -31,4 +31,23 @@ $(document).ready(function() {
 	$('.selection').hide();
 	$('.completed_selection').show();
     });
+
+
+    $('.deals_form_button').click(function() {
+	var str = window.location.href;
+	var beginIndex = str.indexOf("deals/");
+	var id = beginIndex.substring(beginIndex + 6);
+	var postLoc = '/deals/' + id.toString();
+	$.post(postLoc, $('.edit_deal').serialize());
+	$('.exchange_method_selection').load("http://" + window.location.host + '/deals/' + id.toString() + " .exchange_method_selection", function() {
+	});
+	$('.selling_method_selection').load("http://" + window.location.host + '/deals/' + id.toString() + " .selling_method_selection", function() {
+	});
+	$('.payment_method_selection').load("http://" + window.location.host + '/deals/' + id.toString() + " .payment_method_selection", function() {
+	});
+	$('.completed_selection').load("http://" + window.location.host + '/deals/' + id.toString() + " .completed_selection", function() {
+	});
+	$('.guide_selection').load("http://" + window.location.host + '/deals/' + id.toString() + " .guide_selection", function() {
+	});
+    });
 });

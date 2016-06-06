b163f14103f1a0c4785863e6c4f8911fb0b57186
James Lovejoy <jameslovejoy1@gmail.com>
2016-06-06 13:50:40 -0400

    Remove duplicate products from for you.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,23 +3,24 @@ class StaticPagesController < ApplicationController
   def home
     @top_products = Product.joins(:productviews).order('productviews.views DESC').limit(9)
     if logged_in?
-      searches = current_user.searches.order('`search_relationships`.`frequency` DESC').limit(9).pluck(:search_text)
+      searches = current_user.searches.order('`search_relationships`.`frequency` DESC').limit(3).pluck(:search_text)
       results = Array.new
       searches.each do |search_text|
         search = Product.search do
           fulltext search_text do
-            query_phrase_slop 3
+            query_phrase_slop 1
             boost_fields :title => 2.0
             phrase_fields :title => 2.0
             phrase_slop 1
             boost_fields :description => 1.0
           end
-          paginate :page => 1, :per_page => 1
+          paginate :page => 1, :per_page => 3
           with :sold, false
           with :hold, false
           with :activated, true
         end
         results += search.results
+        results = results.uniq
       end
       if results.count < 9
         product = current_user.productviews.order(views: :desc).limit(1).pluck(:product_id)

e4e48e420737769251332010605a449a7f3f2b4f
Ubuntu <ubuntu@ip-172-31-52-19.ec2.internal>
2016-06-06 20:50:09 -0400

    Fix deals js.

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -327,7 +327,7 @@ $(document).ready(function() {
 	var postLoc = '/deals/' + id.toString();
 	updateDeals(postLoc, $(this).parent('form'), function() {
 	    $('.method_selection').load("https://" + window.location.host + '/deals/' + id.toString() + " .inner_method_selection", function() {
-	    )};
+	    });
 	    $('.exchange_method_selection').load("https://" + window.location.host + '/deals/' + id.toString() + " .inner_exchange_method_selection", function() {
 	    });
 	    $('.selling_method_selection').load("https://" + window.location.host + '/deals/' + id.toString() + " .inner_selling_method_selection", function() {

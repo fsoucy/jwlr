6d836acae2d9cf7abe8c85b9ea7cf08923f7fc77
James Lovejoy <jameslovejoy1@gmail.com>
2016-07-12 00:37:21 -0400

    Migrate database, update schema and remove stray debugger.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,6 @@ class NotificationsController < ApplicationController
   before_action :correct_user_notification, only: [:update]
 
   def update
-    debugger
     notification = Notification.find(params[:id])
     notification.update_attributes(notification_params)
     redirect_to notifications_path(notification.user)
